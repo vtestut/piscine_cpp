66a47c55c07f66603bd6d65857ab32a9892f6e9a
Virgile <virgile.testut@gmail.com>
2023-12-13 20:38:09 +0100

    ex01

---
+++ CPP00/ex00/megaphone.cpp
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ex00.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vtestut <vtestut@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/13 20:33:41 by vtestut           #+#    #+#             */
+/*   Updated: 2023/12/13 20:33:42 by vtestut          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <cctype>
+
+int main(int argc, char *argv[])
+{
+    if (argc >= 2)
+    {
+        for (int i = 1; i < argc; i++)
+        {
+            std::string chaine = argv[i];
+            for (size_t j = 0; j < chaine.length(); j++)
+                chaine[j] = std::toupper(chaine[j]);
+
+            std::cout << chaine;
+            if (i == argc - 1)
+                std::cout << std::endl;
+        }
+    }
+    else
+        std::cerr << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+    return (0);
+}
