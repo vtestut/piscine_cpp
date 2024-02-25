bf8c24c734ab4843366ab7eeba88b144f06396c1
Virg macOS <virgile.testut@gmail.com>
2024-02-25 04:38:42 +0100

    newClass

--- newClass/newClass.cpp
+++ newClass/newClass.cpp
@@ -6,52 +6,69 @@
 /*   By: v <v@student.42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/25 02:47:32 by v                 #+#    #+#             */
-/*   Updated: 2024/02/25 03:16:32 by v                ###   ########.fr       */
+/*   Updated: 2024/02/25 04:35:51 by v                ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "newClass.hpp"
+#include "NewClass.hpp"
 
 /* ************************************************************************** */
 /*									PUBBLIC									  */
 /* ************************************************************************** */
 
 
-void NewClass::generateFiles(const std::string& headerFile, const std::string& sourceFile, const std::string& makefile) 
-{
+void NewClass::generateFiles(const std::string& headerFile, const std::string& sourceFile, bool do_makefile) {
 	std::string headerPath = "srcs/header";
 	std::string sourcePath = "srcs/source";
 	std::string makefilePath = "srcs/Makefile";
 
 	_generateHeader(headerPath, headerFile);
 	_generateSource(sourcePath, sourceFile);
-	_generateMakefile(makefilePath, makefile);
+	
+    if (!std::filesystem::exists("CLASS")) {
+        if (!std::filesystem::create_directory("CLASS")) {
+            std::cerr << "Error: Unable to create directory 'CLASS'\n";
+            return;
+        }
+    }
+
+    if (!std::filesystem::exists(headerPath)) {
+        std::cerr << "Error: Header directory '" << headerPath << "' does not exist\n";
+        return;
+    }
+
+    if (!std::filesystem::exists(sourcePath)) {
+        std::cerr << "Error: Source directory '" << sourcePath << "' does not exist\n";
+        return;
+    }
+
+	if (do_makefile == true)
+		_generateMakefile(makefilePath);
 }
+
 
 /* ************************************************************************** */
 /*									PRIVATE									  */
 /* ************************************************************************** */
 
-void NewClass::_generateHeader(const std::string& path, const std::string& filename) 
-{
+
+void NewClass::_generateHeader(const std::string& path, const std::string& filename) {
 	std::ifstream inputFile(path);
-	std::ofstream outputFile(filename + ".hpp");
+	std::ofstream outputFile("CLASS/" + filename + ".hpp");
 	outputFile << inputFile.rdbuf();
-	std::cout << "Header file generated: " << filename << ".hpp\n";
+	std::cout << "Header file generated: " << filename << ".hpp" << std::endl;
 }
 
-void NewClass::_generateSource(const std::string& path, const std::string& filename) 
-{
+void NewClass::_generateSource(const std::string& path, const std::string& filename) {
 	std::ifstream inputFile(path);
-	std::ofstream outputFile(filename + ".cpp");
+	std::ofstream outputFile("CLASS/" + filename + ".cpp");
 	outputFile << inputFile.rdbuf();
-	std::cout << "Source file generated: " << filename << ".cpp\n";
+	std::cout << "Source file generated: " << filename << ".cpp" << std::endl;
 }
 
-void NewClass::_generateMakefile(const std::string& path, const std::string& filename) 
-{
+void NewClass::_generateMakefile(const std::string& path) {
 	std::ifstream inputFile(path);
-	std::ofstream outputFile(filename);
+	std::ofstream outputFile("CLASS/Makefile");
 	outputFile << inputFile.rdbuf();
-	std::cout << "Makefile generated: " << filename << "\n";
+	std::cout << "Makefile generated" << std::endl;
 }
--- newClass/newClass.hpp
+++ newClass/newClass.hpp
@@ -6,7 +6,7 @@
 /*   By: v <v@student.42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/25 02:47:32 by v                 #+#    #+#             */
-/*   Updated: 2024/02/25 03:18:17 by v                ###   ########.fr       */
+/*   Updated: 2024/02/25 04:16:39 by v                ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,19 +18,18 @@
 #include <string>
 #include <filesystem>
 
+
 class NewClass {
 
 public:
 
-    void generateFiles(	const std::string& headerFile, \
-						const std::string& sourceFile, \
-						const std::string& makefile);
+    void generateFiles(	const std::string& headerFile, const std::string& sourceFile, bool  do_makefile);
 
 private:
 
     void _generateHeader(const std::string& path, const std::string& filename);
     void _generateSource(const std::string& path, const std::string& filename);
-    void _generateMakefile(const std::string& path, const std::string& filename);
+    void _generateMakefile(const std::string& path);
 };
 
 #endif
